110ef01cdc4815114a77eed5f46e106e2d244af7
Rene Barrera <rene.barrera1022@gmail.com>
2019-09-11 09:27:36 -0500

    RecorridoMatriz

---
+++ Console/RecorridoMatriz/src/recorridomatriz/RecorridoMatriz.java
@@ -0,0 +1,20 @@
+package recorridomatriz;
+
+public class RecorridoMatriz {
+
+    public static void main(String[] args) {
+        int[][] myMatriz = {{1,3,5,7},{4,7,9,7},{2,6,8,0},{2,4,5,2}};
+        
+        for(int i=0; i< myMatriz.length;i++){
+            for(int j=0; j<myMatriz[i].length;j++){
+                if(i % 2 == 0){
+                    System.out.print(myMatriz[j][i]);
+                }else{
+                    System.out.print(myMatriz[myMatriz.length -j - 1][i]);
+                }
+            }
+        }
+        
+    }
+    
+}
