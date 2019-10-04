c13f7cdbbb2f41f02bb3a1f5ac7c1eacd0b464b2
Rene Barrera <rene.barrera1022@gmail.com>
2019-10-04 08:15:13 -0500

    Subiendo archivo Operadores.java

---
+++ Console/2DArray/src/pkg2darray/Main.java
@@ -0,0 +1,53 @@
+package pkg2darray;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+        
+        for (int y = 0; y < arr.length; y++) {
+
+            for (int x = 0; x < arr.length; x++) {
+                
+            }
+            
+        }
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\reneb\\2DArray.txt"));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ Console/Operadores/src/operadores/Operadores.java
@@ -0,0 +1,99 @@
+package operadores;
+
+import javax.swing.JOptionPane;
+
+public class Operadores {
+
+    private float valor1;
+    private float valor2;
+    private String operador;
+    private float res;
+    private boolean res2;
+
+    public void setOperadores(String valor1, String valor2, String operador) {
+        this.valor1 = Float.parseFloat(valor1);
+        this.valor2 = Float.parseFloat(valor2);
+        this.operador = operador;
+
+        switch (operador) {
+            case "+":
+                res = this.valor1 + this.valor2;
+                System.out.println("Elegiste el operador aritmético '+'");
+                System.out.println("La suma " + this.valor1 + " + " + this.valor2 + " es: " + res);
+                break;
+            case "-":
+                res = this.valor1 - this.valor2;
+                System.out.println("Elegiste el operador aritmético '-'");
+                System.out.println("La resta " + this.valor1 + " - " + this.valor2 + " es: " + res);
+                break;
+            case "*":
+                res = this.valor1 * this.valor2;
+                System.out.println("Elegiste el operador aritmético '*'");
+                System.out.println("La multiplicación " + this.valor1 + " * " + this.valor2 + " es: " + res);
+                break;
+            case "/":
+                res = this.valor1 / this.valor2;
+                System.out.println("Elegiste el operador aritmético '/'");
+                System.out.println("La división " + this.valor1 + " / " + this.valor2 + " es: " + res);
+                break;
+            case "%":
+                res = this.valor1 % this.valor2;
+                System.out.println("Elegiste el operador aritmético '%'");
+                System.out.println("El residuio " + this.valor1 + " % " + this.valor2 + " es: " + res);
+                break;
+        }
+    }
+
+    public boolean setOperadores2(String valor1, String valor2, String operador) {
+        res2 = false;
+        this.valor1 = Float.parseFloat(valor1);
+        this.valor2 = Float.parseFloat(valor2);
+        this.operador = operador;
+
+        switch (operador) {
+            case "<":
+                if (this.valor1 < this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case ">":
+                if (this.valor1 > this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case "<=":
+                if (this.valor1 <= this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case ">=":
+                if (this.valor1 >= this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case "==":
+                if (this.valor1 == this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case "!=":
+                if (this.valor1 != this.valor2) {
+                    res2 = true;
+                }
+                break;
+            case "&&":
+                JOptionPane.showMessageDialog(null, "La condición será: si ambos número son mayores a 10");
+                if (this.valor1 > 10 && this.valor2 > 10) {
+                    res2 = true;
+                }
+                break;
+            case "||":
+                JOptionPane.showMessageDialog(null, "La condición será: si uno de los dos número es mayor a 10");
+                if (this.valor1 > 10 || this.valor2 < 10) {
+                    res2 = true;
+                }
+                break;
+        }
+        return res2;
+    }
+}
