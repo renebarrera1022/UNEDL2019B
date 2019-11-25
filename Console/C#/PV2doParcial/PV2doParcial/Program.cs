7a2c9b672f7cb7cd1096ffa8f3451021d7b4ed9a
Rene Barrera <rene.barrera1022@gmail.com>
2019-11-25 09:53:48 -0600

    Examen 2do Parcial PVIII

---
+++ Console/C#/PV2doParcial/PV2doParcial/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PV2doParcial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        Stack<string> pila = new Stack<string>(); ;
+        String validar;
+
+             do
+             {
+                 Console.WriteLine("Para salir presiona la tecla 0");
+                 Console.WriteLine("Ingresa los paréntesis y/o corchetes de una ecuación: ");
+                 validar = Console.ReadLine();
+                 if (validar == ")" || validar == "}" || validar == "]")
+                 {
+                     pila.Push(validar);
+                 }
+                 else if (validar == "[")
+                 {
+                     for (int i = 0; i < pila.Count; i++)
+                     {
+                         if (pila.Contains("]"))
+                         {
+                             pila.remove(i);
+                         }
+                     }
+                 }
+                 else if (validar == "{")
+                 {
+                     for (int i = pila.Count; i >= 0; i--)
+                     {
+                         if (pila.Get(i) == "}")
+                         {
+                             pila.Pop();
+                         }
+                     }
+                 }
+                 else if (validar == "(")
+                 {
+                     for (int i = 0; i < pila.Count; i++)
+                     {
+                         if (pila.get(i) == ")")
+                         {
+                             pila.remove(i);
+                         }
+                     }
+                 }
+                 else if (validar != "0")
+                 {
+                     Console.Write("Valor inválido");
+                 }
+             } while (validar != "0");
+             if (pila.Count >= 1)
+             {
+                 Console.Write("--------------- SISTEMA INCONGRUENTE ---------------");
+             }
+             else
+             {
+                 Console.Write("--------------- SISTEMA CONGRUENTE ---------------");
+             }
+        }
+    }
+}
