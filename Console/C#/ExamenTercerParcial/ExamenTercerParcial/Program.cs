5af4af13610d349f1544888bee919db800239417
Rene Barrera <rene.barrera1022@gmail.com>
2020-01-27 10:28:30 -0600

    Subiendo examen 3er parcial

---
+++ Console/C#/ExamenTercerParcial/ExamenTercerParcial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExamenTercerParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 6, 4, 1 };
+            int temp;
+            int n = 0;
+            for (int j = 0; j <= a.Length - 2; j++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        temp = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = temp;
+                        n++;
+                    }
+                }
+            }
+            Console.WriteLine("Nuevo orden:");
+            Console.Write("[");
+            foreach (int p in a) { 
+                Console.Write(p + ", ");
+            }
+            Console.Write("]");
+            Console.Write("\nPrimer elemento: " + a[0]);
+            Console.Write("\nUltimo elemento: " + a[2]);
+            Console.Write("\nPermutas: " + n);
+        }
+
+
+    }
+}
